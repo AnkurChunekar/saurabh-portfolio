cbce5eb4784d3bea9c21207af4f91f92eeaa917d
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-06 22:41:56 +0530

    feat: add hero section

---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const heroText =
+  "Hi! I am Saurabh Chunekar. I am a product designer from Mumbai, India, focused on websites and mobile application design.";
+
+const Hero = () => {
+  return (
+    <h1 className="font-secondary font-bold text-6xl mt-24">{heroText}</h1>
+  );
+};
+
+export default Hero;
