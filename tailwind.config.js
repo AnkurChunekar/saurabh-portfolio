e4ac98b5fdfbe58e64a146c2a91266821f73c98e
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-10 17:27:14 +0530

    feat: add tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      dark: "#0E0F13",
-      font: "#ECEBF0",
-      cta: "#FF2E00",
-    },
     fontFamily: {
       primary: ["Inter", "sans-serif"],
       secondary: ["Space Grotesk", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      lineHeight: {
+        16: "64px",
+      },
+      colors: {
+        dark: "#0E0F13",
+        font: "#ECEBF0",
+        darkGray: "#26262B",
+        cta: "#FF2E00",
+        cardTag: "rgba(247, 246, 251, 0.5)",
+      },
+      aspectRatio: {
+        "2/3": "2 / 3",
+        "9/16": "9/16",
+      },
+    },
   },
   plugins: [],
 };
