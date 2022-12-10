de770844c6d296c6ea8e646066d2e2b8040633be
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-10 17:27:57 +0530

    feat: add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul className="flex items-center justify-start text-xl font-semibold gap-x-8 py-20">
+        <li>Home</li>
+        <li>Contact</li>
+        <li>Linkedin</li>
+        <li>Twitter</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
