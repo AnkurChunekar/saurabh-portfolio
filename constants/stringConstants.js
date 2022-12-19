97555cc25037dc6d1233330e58f43c169ab5fa1e
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-12-19 21:13:29 +0530

    feat: add new project data and images

--- constants/stringConstants.js
+++ constants/stringConstants.js
@@ -12,10 +12,9 @@ const PROJECTS = [
     src: nourishImg,
   },
   {
-    title: "Find a Tutor",
-    description: `Find a tutor is an app where parents/guardians can
-        book private tutor services for their child`,
-    subtitle: "Mobile App Design / UI/UX",
+    title: "Master Tutor: Mobile App Design",
+    description: `Master Tutor is an app where you can hire private tutors in your area without worrying about their qualification and experience.`,
+    subtitle: "Mobile App / UI/UX",
     id: "2",
     src: tutorImg,
   },
@@ -62,19 +61,19 @@ const FOOTER_LINKS = [
 const ABOUT_STRINGS = [
   {
     id: "1",
-    text: "Hello! I am Saurabh. I am a self-taught designer from Mumbai, India. I design landing page websites and mobile applications which are usable for the customers and making sure they have great user experience while on it.",
+    text: "Hello! I am Saurabh. I am a self-taught product designer from Mumbai, India. I design landing pages, websites and mobile applications.",
   },
   {
     id: "2",
-    text: "I have previously worked as a UI/UX design Intern at a design and branding agency from Hyderabad. As an intern I helped my team in conducting user research and deriving insights from the research. Before that I worked as a graphic designer at a mass customization company in Mumbai.",
+    text: "I have previously worked as a UI/UX design Intern at a design and branding agency from Hyderabad. Before that I worked as a graphic designer at a mass customization company in Mumbai.",
   },
   {
     id: "3",
-    text: "I am always on the lookout for new and exciting product design opportunities and making new friends while working on some complex problems together.",
+    text: "I am looking for new and exciting product design opportunities and making new friends while working on some complex problems together.",
   },
   {
     id: "4",
-    text: "Other than design, I am passionate about fitness, biochemistry, health and nutrition. Doing workout 4-5 times a week gives me joy and a sense of achievement, also helping me overcome stress and anxiety and I am always open for conversation about the same.",
+    text: "Other than design, I am passionate about fitness, biochemistry, health and nutrition. Doing workout 4-5 times a week gives me joy and a sense of achievement, also helps me overcome stress and anxiety.",
   },
 ];
 
